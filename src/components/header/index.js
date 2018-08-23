d7674a760f25665f1d21905c24eb2fcda1fc5ce9
jamesbillard <jamesbillard12@hotmail.com>
2018-08-23 12:22:03 -0700

    header with logo has been added. needs ham

---
+++ src/components/header/index.js
@@ -0,0 +1,14 @@
+import './header.scss'
+import React from 'react'
+
+class Header extends React.Component {
+	render() {
+		return (
+			<div className="header">
+				<div className="header__logo" />
+			</div>
+		)
+	}
+}
+
+export default Header
--- src/components/landing/index.js
+++ src/components/landing/index.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import Header from '../header'
 
 class Landing extends React.Component {
 	render() {
-		return <h1>Hello World</h1>
+		return (
+			<div>
+				<Header />
+			</div>
+		)
 	}
 }
 

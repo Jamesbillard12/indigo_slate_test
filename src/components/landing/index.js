383fad2509b100b1a924e23a97921b0f929f7a46
jamesbillard <jamesbillard12@hotmail.com>
2018-08-23 19:43:31 -0700

    section items seem to be working

--- src/components/landing/index.js
+++ src/components/landing/index.js
@@ -11,21 +11,108 @@ class Landing extends React.Component {
 			{
 				id: 0,
 				name: 'my schedule & biddings',
-				categories: [1, 2, 3],
+				categories: [
+					{
+						parentId: 0,
+						id: 0,
+						name: 'my schedule',
+						pages: []
+					},
+					{
+						parentId: 0,
+						id: 1,
+						name: 'my biddings',
+						pages: []
+					}
+				],
 				icon: 'fas fa-calendar-alt'
 			},
-			{ id: 1, name: 'safety', categories: [1, 2, 3, 4], icon: 'fas fa-lock' },
+			{
+				id: 1,
+				name: 'safety',
+				categories: [
+					{
+						parentId: 1,
+						id: 0,
+						name: 'reporting',
+						pages: [
+							{
+								grandparentId: 1,
+								parentId: 0,
+								id: 0,
+								name: '1-21 injury reporting'
+							},
+							{
+								grandparentId: 1,
+								parentId: 0,
+								id: 1,
+								name: 'ASAP reporting'
+							},
+							{
+								grandparentId: 1,
+								parentId: 0,
+								id: 2,
+								name: 'general ASAP information'
+							},
+							{
+								grandparentId: 1,
+								parentId: 0,
+								id: 3,
+								name: 'flight attendant incident report'
+							}
+						]
+					},
+					{
+						parentId: 1,
+						id: 1,
+						name: 'agriculture and customs',
+						pages: [
+							{
+								grandparentId: 1,
+								parentId: 1,
+								id: 0,
+								name: 'meats'
+							},
+							{
+								grandparentId: 1,
+								parentId: 1,
+								id: 1,
+								name: 'fruits and vegetables'
+							},
+							{
+								grandparentId: 1,
+								parentId: 1,
+								id: 2,
+								name: 'alcohol'
+							}
+						]
+					},
+					{
+						parentId: 1,
+						id: 2,
+						name: 'known crewmember',
+						pages: []
+					},
+					{
+						parentId: 1,
+						id: 3,
+						name: 'product safety data search',
+						pages: []
+					}
+				],
+				icon: 'fas fa-lock'
+			},
 			{ id: 2, name: 'training', categories: [], icon: 'fas fa-dumbbell' },
 			{
 				id: 3,
 				name: 'administration',
-				categories: [],
+				categories: [1, 2, 3, 4],
 				icon: 'fas fa-user-tie'
 			},
 			{
 				id: 4,
 				name: 'catering & brand',
-				categories: [],
+				categories: [1, 2, 3, 4],
 				icon: 'fas fa-utensils'
 			},
 			{ id: 5, name: 'hotels', categories: [], icon: 'fas fa-hotel' },
--- src/components/section-item/index.js
+++ src/components/section-item/index.js
@@ -3,7 +3,6 @@ import React from 'react'
 import { renderIf } from '../../../lib/util.js'
 
 const SectionItem = props => {
-	console.log(props.section)
 	return (
 		<div className="section-item">
 			<div className="section-item__icon">
@@ -15,7 +14,7 @@ const SectionItem = props => {
 			{renderIf(
 				props.section.categories.length,
 				<div className="section-item__arrow_right">
-					<i class="fas fa-angle-right" />
+					<i className="fas fa-angle-right" />
 				</div>
 			)}
 		</div>

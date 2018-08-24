785af25840738f5c4a49ea5a67ae9f7a6255cc8a
jamesbillard <jamesbillard12@hotmail.com>
2018-08-24 10:04:15 -0700

    handling category open and close arrows

--- src/components/category-item/index.js
+++ src/components/category-item/index.js
@@ -3,30 +3,16 @@ import React from 'react'
 import { renderIf } from '../../../lib/util.js'
 
 class CategoryItem extends React.Component {
-	constructor() {
-		super()
+	state = {
+		categoryOpen: false
 	}
-
-	componentDidMount() {
-		this.handleClick()
+	handleCategoryOpen = () => {
+		this.setState({ categoryOpen: !this.state.categoryOpen })
 	}
-
-	handleClick = () => {
-		const acc = this._acc.children
-		for (let i = 0; i < acc.length; i++) {
-			let a = acc[i]
-			a.onclick = () => a.classList.toggle('active')
-		}
-	}
-
 	render() {
 		console.log(this.props.category.pages)
 		return (
-			<div
-				ref={a => (this._acc = a)}
-				onClick={this.handleClick}
-				className="category-item"
-			>
+			<div onClick={() => this.handleCategoryOpen()} className="category-item">
 				<div
 					// onClick={() => this.props.handleCategoryBodyClick(this.props.category.id)}
 					className="category-item__body"
@@ -41,10 +27,15 @@ class CategoryItem extends React.Component {
 						// onClick={() => this.props.handleCategoryArrowClick(this.props.category.id)}
 						className="category-item__arrow_up"
 					>
-						<i className="fas fa-angle-up" />
+						<i
+							className={
+								this.state.categoryOpen
+									? 'fas fa-angle-down'
+									: 'fas fa-angle-up'
+							}
+						/>
 					</div>
 				)}
-				{this.props.children}
 			</div>
 		)
 	}
